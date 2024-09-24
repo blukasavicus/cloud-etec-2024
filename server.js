e26abd23f975698670d3dc9a0ff06fccb3101a3e
unknown <ANA_23868@ETEC.FURLAN>
2024-09-24 09:58:41 -0300

    24/09

--- server.js
+++ server.js
@@ -1,30 +1,25 @@
-//Vou instalar o express - 
-//Gerenciador de pacotes - NPM
-
-const express = require('express')
+const express = require ('express')
 const server = express()
-
 //Importar para ter acesso ao caminho dos arquivos
 const path = require('path')
-
-//Expor aquivos estáticos
+//Expor arquivos estáticos..
 server.use(express.static('public'))
-server.use(express.json())//Possibilidade de usar JSON
-//Eu desenvolvedor, criei este ENDPOINT 
-server.get('/cadastro', (req,res)=>{
-     res.sendFile(path.join(__dirname, 'views/index.html'))
+server.use(express.json()) //Possibilidade de usar JSON
+//Eu desenvolvedor, criei este ENDPOINT
+//http://localhost:3000/cadastro
+server.get('/cadastro', (req, res) => {
+   res.sendFile(path.join(__dirname, 'views/index.html'))
 })
-
-server.get('/pets', (req,res)=>{
+server.post('/cadastro', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'views/home.html'))
+})
+server.get('/pets', (req,res)=> {
     res.send({
-        name: "nina",
-        idade: "11 anos",
-        peso:"7kg"
+        name: "Meu gato",
+        idade: "4 anos",
+        peso: "300kg"
     })
 })
-
-//Expor para o mundo na porta 3000
 server.listen(3000, () => {
     console.log("Servidor no ar...")
-
 })
